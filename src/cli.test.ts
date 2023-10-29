cb11ec5991d8c12fbef2a95090d5b0a4bbc30cbd
Lily Skye <me@suchipi.com>
2023-10-29 09:45:26 -0600

    add CLI help test

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -108,3 +108,96 @@ test("sample cli run", async () => {
     }
   `);
 });
+
+test("help", async () => {
+  const run1 = spawn("node", [cliPath, "--help"]);
+  await run1.completion;
+  expect(cleanRunResult(run1.result)).toMatchInlineSnapshot(`
+    {
+      "code": 0,
+      "error": false,
+      "stderr": "",
+      "stdout": "# resolve-everything
+
+    resolve-everything is a tool which walks through the module dependency tree of
+    a JavaScript/TypeScript project and gives you data about all the relationships.
+
+    ## Usage
+
+    $ resolve-everything --entrypoint ./some-file.js
+
+    ## Options
+
+    --entrypoint (path):
+      The place to start walking the module graph.
+      This is the only required option.
+
+    --skip (RegExp|\\"none\\"):
+      Which files not to parse and find requires/imports in.
+      Defaults to /node_modules/. Specify \\"none\\" to parse everything.
+
+    --json (boolean):
+      Whether to output json or human-readable data.
+      Default value varies depending on if stdout is a tty.
+
+    --only-entrypoint (boolean):
+      If true, only the imports/requires in the entrypoint file will be resolved,
+      and no other files will be walked over.
+
+
+    --sort (boolean):
+      If true, results will be sorted lexicographically. If you want results to be
+      stable, you should enable this, as the order is non-deterministic otherwise
+      (because we crawl the filesystem concurrently).
+
+    --resolver (path):
+      Module which exports a JS function that locates modules. Uses a resolver
+      format compatible with https://github.com/suchipi/kame, namely:
+
+      export const resolve: {
+        (id: string, fromFilePath: string): string;
+        async?: (id: string, fromFilePath: string) => Promise<string>;
+      }
+
+      The 'async' property is optional; if not present, the normal synchronous
+      resolver will be used.
+
+    --full-errors (boolean):
+      If reading, parsing, traversal, or resolution errors occur, print as much
+      error information as possible.
+
+    ## Output
+
+    The program prints JSON shaped like:
+
+    {
+      \\"<full-path-to-some-file.js>\\": {
+        \\"id\\": \\"<full-path-to-some-file.js>\\",
+        \\"requests\\": {
+          // key is require/import string that appeared in the source code
+          // value is full path to the file that refers to
+          \\"./something\\": \\"<full-path-to-something>\\",
+          \\"react\\": \\"<full-path-to-react>\\",
+          \\"../somewhere/yeah\\": \\"<full-path-to-yeah>\\",
+        },
+        \\"dependencies\\": [
+          \\"<full-path-to-something>\\",
+          \\"<full-path-to-react>\\",
+          \\"<full-path-to-yeah>\\",
+        ]
+      },
+      \\"<full-path-to-something>\\": { ... },
+      \\"<full-path-to-yeah>\\": { ... },
+      ...
+    }
+
+    resolve-everything was made with <3 by Lily Skye. Though it likely can't resolve
+    *all* of your problems, maybe it can resolve one of them.
+    ",
+    }
+  `);
+
+  const run2 = spawn("node", [cliPath, "-h"]);
+  await run2.completion;
+  expect(run2.result).toEqual(run1.result);
+});
