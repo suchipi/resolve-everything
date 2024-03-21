8283629d65fe2fcddc1b95eebc45652748291c46
Lily Skye <me@suchipi.com>
2024-03-20 23:38:34 -0600

    bump deps

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -19,88 +19,88 @@ test("sample cli run", async () => {
       "error": false,
       "stderr": "",
       "stdout": "{
-      \\"<rootDir>/src/debug-logger.ts\\": {
-        \\"id\\": \\"<rootDir>/src/debug-logger.ts\\",
-        \\"requests\\": {
-          \\"debug\\": \\"<rootDir>/node_modules/debug/src/index.js\\"
+      "<rootDir>/src/debug-logger.ts": {
+        "id": "<rootDir>/src/debug-logger.ts",
+        "requests": {
+          "debug": "<rootDir>/node_modules/debug/src/index.js"
         },
-        \\"dependencies\\": [
-          \\"<rootDir>/node_modules/debug/src/index.js\\"
+        "dependencies": [
+          "<rootDir>/node_modules/debug/src/index.js"
         ]
       },
-      \\"<rootDir>/src/default-resolver.ts\\": {
-        \\"id\\": \\"<rootDir>/src/default-resolver.ts\\",
-        \\"requests\\": {
-          \\"node:module\\": \\"external:node:module\\",
-          \\"node:path\\": \\"external:node:path\\",
-          \\"node:util\\": \\"external:node:util\\",
-          \\"resolve\\": \\"<rootDir>/node_modules/resolve/index.js\\"
+      "<rootDir>/src/default-resolver.ts": {
+        "id": "<rootDir>/src/default-resolver.ts",
+        "requests": {
+          "node:module": "external:node:module",
+          "node:path": "external:node:path",
+          "node:util": "external:node:util",
+          "resolve": "<rootDir>/node_modules/resolve/index.js"
         },
-        \\"dependencies\\": [
-          \\"external:node:module\\",
-          \\"external:node:path\\",
-          \\"external:node:util\\",
-          \\"<rootDir>/node_modules/resolve/index.js\\"
+        "dependencies": [
+          "external:node:module",
+          "external:node:path",
+          "external:node:util",
+          "<rootDir>/node_modules/resolve/index.js"
         ]
       },
-      \\"<rootDir>/src/module.ts\\": {
-        \\"id\\": \\"<rootDir>/src/module.ts\\",
-        \\"requests\\": {
-          \\"./debug-logger\\": \\"<rootDir>/src/debug-logger.ts\\",
-          \\"./types\\": \\"<rootDir>/src/types.ts\\",
-          \\"equivalent-exchange\\": \\"<rootDir>/node_modules/equivalent-exchange/dist/index.js\\",
-          \\"node:fs\\": \\"external:node:fs\\",
-          \\"pretty-print-ast\\": \\"<rootDir>/node_modules/pretty-print-ast/dist/index.js\\"
+      "<rootDir>/src/module.ts": {
+        "id": "<rootDir>/src/module.ts",
+        "requests": {
+          "./debug-logger": "<rootDir>/src/debug-logger.ts",
+          "./types": "<rootDir>/src/types.ts",
+          "equivalent-exchange": "<rootDir>/node_modules/equivalent-exchange/dist/index.js",
+          "node:fs": "external:node:fs",
+          "pretty-print-ast": "<rootDir>/node_modules/pretty-print-ast/dist/index.js"
         },
-        \\"dependencies\\": [
-          \\"<rootDir>/src/debug-logger.ts\\",
-          \\"<rootDir>/src/types.ts\\",
-          \\"<rootDir>/node_modules/equivalent-exchange/dist/index.js\\",
-          \\"external:node:fs\\",
-          \\"<rootDir>/node_modules/pretty-print-ast/dist/index.js\\"
+        "dependencies": [
+          "<rootDir>/src/debug-logger.ts",
+          "<rootDir>/src/types.ts",
+          "<rootDir>/node_modules/equivalent-exchange/dist/index.js",
+          "external:node:fs",
+          "<rootDir>/node_modules/pretty-print-ast/dist/index.js"
         ]
       },
-      \\"<rootDir>/src/types.ts\\": {
-        \\"id\\": \\"<rootDir>/src/types.ts\\",
-        \\"requests\\": {},
-        \\"dependencies\\": []
+      "<rootDir>/src/types.ts": {
+        "id": "<rootDir>/src/types.ts",
+        "requests": {},
+        "dependencies": []
       },
-      \\"<rootDir>/src/walk.ts\\": {
-        \\"id\\": \\"<rootDir>/src/walk.ts\\",
-        \\"requests\\": {
-          \\"./debug-logger\\": \\"<rootDir>/src/debug-logger.ts\\",
-          \\"./default-resolver\\": \\"<rootDir>/src/default-resolver.ts\\",
-          \\"./module\\": \\"<rootDir>/src/module.ts\\",
-          \\"./types\\": \\"<rootDir>/src/types.ts\\",
-          \\"./walker\\": \\"<rootDir>/src/walker.ts\\",
-          \\"node:path\\": \\"external:node:path\\"
+      "<rootDir>/src/walk.ts": {
+        "id": "<rootDir>/src/walk.ts",
+        "requests": {
+          "./debug-logger": "<rootDir>/src/debug-logger.ts",
+          "./default-resolver": "<rootDir>/src/default-resolver.ts",
+          "./module": "<rootDir>/src/module.ts",
+          "./types": "<rootDir>/src/types.ts",
+          "./walker": "<rootDir>/src/walker.ts",
+          "node:path": "external:node:path"
         },
-        \\"dependencies\\": [
-          \\"<rootDir>/src/debug-logger.ts\\",
-          \\"<rootDir>/src/default-resolver.ts\\",
-          \\"<rootDir>/src/module.ts\\",
-          \\"<rootDir>/src/types.ts\\",
-          \\"<rootDir>/src/walker.ts\\",
-          \\"external:node:path\\"
+        "dependencies": [
+          "<rootDir>/src/debug-logger.ts",
+          "<rootDir>/src/default-resolver.ts",
+          "<rootDir>/src/module.ts",
+          "<rootDir>/src/types.ts",
+          "<rootDir>/src/walker.ts",
+          "external:node:path"
         ]
       },
-      \\"<rootDir>/src/walker.ts\\": {
-        \\"id\\": \\"<rootDir>/src/walker.ts\\",
-        \\"requests\\": {
-          \\"./debug-logger\\": \\"<rootDir>/src/debug-logger.ts\\",
-          \\"./module\\": \\"<rootDir>/src/module.ts\\",
-          \\"./types\\": \\"<rootDir>/src/types.ts\\",
-          \\"node:events\\": \\"external:node:events\\",
-          \\"node:path\\": \\"external:node:path\\",
-          \\"parallel-park\\": \\"<rootDir>/node_modules/parallel-park/dist/index.js\\"
+      "<rootDir>/src/walker.ts": {
+        "id": "<rootDir>/src/walker.ts",
+        "requests": {
+          "./debug-logger": "<rootDir>/src/debug-logger.ts",
+          "./module": "<rootDir>/src/module.ts",
+          "./types": "<rootDir>/src/types.ts",
+          "node:events": "external:node:events",
+          "node:path": "external:node:path",
+          "parallel-park": "<rootDir>/node_modules/parallel-park/dist/index.js"
         },
-        \\"dependencies\\": [
-          \\"<rootDir>/src/debug-logger.ts\\",
-          \\"<rootDir>/src/module.ts\\",
-          \\"<rootDir>/src/types.ts\\",
-          \\"external:node:events\\",
-          \\"external:node:path\\",
-          \\"<rootDir>/node_modules/parallel-park/dist/index.js\\"
+        "dependencies": [
+          "<rootDir>/src/debug-logger.ts",
+          "<rootDir>/src/module.ts",
+          "<rootDir>/src/types.ts",
+          "external:node:events",
+          "external:node:path",
+          "<rootDir>/node_modules/parallel-park/dist/index.js"
         ]
       }
     }
@@ -132,9 +132,9 @@ test("help", async () => {
       The place to start walking the module graph.
       This is the only required option.
 
-    --skip (RegExp|\\"none\\"):
+    --skip (RegExp|"none"):
       Which files not to parse and find requires/imports in.
-      Defaults to /node_modules/. Specify \\"none\\" to parse everything.
+      Defaults to /node_modules/. Specify "none" to parse everything.
 
     --json (boolean):
       Whether to output json or human-readable data.
@@ -171,23 +171,23 @@ test("help", async () => {
     The program prints JSON shaped like:
 
     {
-      \\"<full-path-to-some-file.js>\\": {
-        \\"id\\": \\"<full-path-to-some-file.js>\\",
-        \\"requests\\": {
+      "<full-path-to-some-file.js>": {
+        "id": "<full-path-to-some-file.js>",
+        "requests": {
           // key is require/import string that appeared in the source code
           // value is full path to the file that refers to
-          \\"./something\\": \\"<full-path-to-something>\\",
-          \\"react\\": \\"<full-path-to-react>\\",
-          \\"../somewhere/yeah\\": \\"<full-path-to-yeah>\\",
+          "./something": "<full-path-to-something>",
+          "react": "<full-path-to-react>",
+          "../somewhere/yeah": "<full-path-to-yeah>",
         },
-        \\"dependencies\\": [
-          \\"<full-path-to-something>\\",
-          \\"<full-path-to-react>\\",
-          \\"<full-path-to-yeah>\\",
+        "dependencies": [
+          "<full-path-to-something>",
+          "<full-path-to-react>",
+          "<full-path-to-yeah>",
         ]
       },
-      \\"<full-path-to-something>\\": { ... },
-      \\"<full-path-to-yeah>\\": { ... },
+      "<full-path-to-something>": { ... },
+      "<full-path-to-yeah>": { ... },
       ...
     }
 
